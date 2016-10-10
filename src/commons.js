7ee6217cfdff7674284db70363d523dcde7f65a0
Lech Piechota <lech_piechota@epam.com>
2016-10-10 13:34:20 +0200

    uproszczenie skryptu js

--- src/commons.js
+++ src/commons.js
@@ -1,12 +1,4 @@
-function injectTemplate(templateName){
-	$.ajax({
-		url: 'templates/'+templateName+'.html',
-		method: 'GET',
-		success: function(content) {
-			document.getElementById(templateName).innerHTML = content;
-		}
-	});
-}
-
-injectTemplate('header');
-injectTemplate('bottom');
+$(function(){
+  $("#header").load("templates/header.html");
+  $("#bottom").load("templates/bottom.html");
+});
