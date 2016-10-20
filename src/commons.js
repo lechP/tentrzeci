ee8237ab29286dbb37a955e7d9bd9c4c1ae9c05f
Lech Piechota <lech_piechota@epam.com>
2016-10-20 19:36:17 +0200

    poprawka pod safari (które nie ogarnia URLSearchParams)

--- src/commons.js
+++ src/commons.js
@@ -1,9 +1,8 @@
 $(function() {
-    getParams = new URLSearchParams(window.location.search)
-
+    var isAfterRegister = getUrlParameter('after_register');
     $("#header").load("templates/header.html");
 
-    if(getParams.has('after_register')) {
+    if(isAfterRegister) {
         $("#content").load("templates/after_register.html");
     } else {
         $("#content").load("templates/o_rekolekcjach.html");
@@ -16,3 +15,18 @@ $(function() {
 function loadContent(contentName){
     $("#content").load("templates/"+contentName+".html");
 }
+
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
